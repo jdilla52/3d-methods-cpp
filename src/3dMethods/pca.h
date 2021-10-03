650b55f3e9b599d32ce6b20d878c3e645486f88f
jacques <jacques.perrault@branch.technology>
2021-10-03 13:29:14 -0400

    added pca page 52

--- src/3dMethods/pca.h
+++ src/3dMethods/pca.h
@@ -5,7 +5,59 @@
 #ifndef LAMBDARUNNER_PCA_H
 #define LAMBDARUNNER_PCA_H
 
+#include <iostream>
+#include <Eigen/Dense>
 
+using namespace Eigen;
+using namespace std;
 
+ArrayXXd AverageRows(MatrixXd m) {
+  ArrayXXd R(1, 3);
+  for (int i = 0; i < m.rows(); i++) {
+    R += m.row(i).array();
+  }
+  return R / m.rows();
+}
+
+/// perform pca on a matrix of data
+/// \param mat
+/// \return eigenValues, eigenVectors
+tuple<VectorXd, MatrixXd> Pca(MatrixXd mat) {
+  try {
+    // center data to origin
+    MatrixXd centered = mat.rowwise() - mat.colwise().mean();
+    // create covariance matrix
+    MatrixXd cov = (centered.adjoint() * centered) / double(mat.rows() - 1);
+    // spectral decomp using eigen
+    SelfAdjointEigenSolver<MatrixXd> eigensolver(cov);
+    if (eigensolver.info() != Success) throw runtime_error("failed to solve");
+
+    VectorXd eigen_values = eigensolver.eigenvalues();
+    MatrixXd eigen_vectors = eigensolver.eigenvectors();
+
+    // sort values and vectors - pretty hacky I wish this was in eigen...
+    std::vector<std::tuple<float, VectorXd>> eigen_vectors_and_values;
+
+    for (int i = 0; i < eigen_values.size(); i++) {
+      std::tuple<float, VectorXd> vec_and_val(eigen_values[i], eigen_vectors.row(i));
+      eigen_vectors_and_values.push_back(vec_and_val);
+    }
+    std::sort(eigen_vectors_and_values.begin(), eigen_vectors_and_values.end(),
+              [&](const std::tuple<float, VectorXd> &a, const std::tuple<float, VectorXd> &b) -> bool {
+                return std::get<0>(a) > std::get<0>(b);
+              });
+
+    // replace existing matrices with sorted values
+    for (int i = 0; i < eigen_vectors_and_values.size(); i++) {
+      auto cur = eigen_vectors_and_values[i];
+      eigen_values[i] = std::get<0>(cur);
+      eigen_vectors.row(i) = std::get<1>(cur);
+    }
+    tuple<VectorXd, MatrixXd> final(eigen_values, eigen_vectors);
+    return final;
+  } catch (exception &e) {
+    throw e;
+  }
+}
 
 #endif //LAMBDARUNNER_PCA_H
--- test/3dMethods.cpp
+++ test/3dMethods.cpp
@@ -4,20 +4,17 @@
 #include <catch2/catch.hpp>
 #include <iostream>
 #include <Eigen/Dense>
-SCENARIO("test generate generic reponse", "[utils.cpp]")
+#include <3dMethods/pca.h>
+using namespace Eigen;
+using namespace std;
+
+SCENARIO("pca", "[utils.cpp]")
 {
-//auto test = formResponse("succeeded", "i'm a message", "test_func");
-//std::string valid =
-//        R"({"info":{"functionName":"test_func","info":"i'm some info"},"message":"i'm a message","status":"succeeded"})";
-//
-//REQUIRE(test == valid);
+    MatrixXd mat(3,3);
+    mat << 1, 1, 1, -2, -1, -1, 1.2, 1.2, 1.2;
+    auto j = Pca(mat);
 
+    cout << "eigenValues:\n" << get<0>(j) << endl;
+    cout << "eigen vectors: \n" << get<1>(j) << endl;
 
-    using Eigen::MatrixXd;
-    MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
 }
